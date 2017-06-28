8c7c8cdb8e17f32660e37a21a249ef1c39015a9f
Tim Watson <tiwatson@gmail.com>
2017-06-27 20:06:07 -0600

    Date Picker

--- src/components/nav.js
+++ src/components/nav.js
@@ -7,34 +7,10 @@ export default class Nav extends Component {
     return (
       <div className="ui fixed inverted menu">
         <div className="ui container">
-          <a href="#" className="header item">
-            Project Name
-          </a>
-          <a href="#" className="item">Home</a>
-          <Link to="/">Home</Link>
-          <Link to="/new">New</Link>
-
-          <div className="ui simple dropdown item">
-            Dropdown <i className="dropdown icon"></i>
-            <div className="menu">
-              <a className="item" href="#">Link Item</a>
-              <a className="item" href="#">Link Item</a>
-              <div className="divider"></div>
-              <div className="header">Header Item</div>
-              <div className="item">
-                <i className="dropdown icon"></i>
-                Sub Menu
-                <div className="menu">
-                  <a className="item" href="#">Link Item</a>
-                  <a className="item" href="#">Link Item</a>
-                </div>
-              </div>
-              <a className="item" href="#">Link Item</a>
-            </div>
-          </div>
+          <Link to="/" className="header item">Reserve</Link>
+          <Link to="/new" className="item">New</Link>
         </div>
       </div>
-
     );
   }
 }
--- src/components/requestFormStep2.js
+++ src/components/requestFormStep2.js
@@ -1,20 +1,56 @@
 import React, { Component } from 'react';
-import { Field } from 'redux-form';
 
 import SemanticInput from './semanticInput'
 
+import DayPicker, { DateUtils } from 'react-day-picker';
+import moment from 'moment';
+
+import 'react-day-picker/lib/style.css';
+
+
 export default class RequestFormStep2 extends Component {
+  state = {
+    from: null,
+    to: null,
+  };
+  handleDayClick = day => {
+    const range = DateUtils.addDayToRange(day, this.state);
+    this.setState(range);
+  };
+  handleResetClick = e => {
+    e.preventDefault();
+    this.setState({
+      from: null,
+      to: null,
+    });
+  };
 
   render() {
+    const { from, to } = this.state;
+
     return (
-      <div>
-        <label>Dates</label>
-        <div>
-          <Field
-            name="dates"
-            component={SemanticInput}
-          />
-        </div>
+      <div className="RangeExample">
+        {!from && !to && <p>Please select the <strong>first day</strong>.</p>}
+        {from && !to && <p>Please select the <strong>last day</strong>.</p>}
+        {from &&
+          to &&
+          <p>
+            You chose from
+            {' '}
+            {moment(from).format('L')}
+            {' '}
+            to
+            {' '}
+            {moment(to).format('L')}
+            .
+            {' '}<a href="." onClick={this.handleResetClick}>Reset</a>
+          </p>}
+        <DayPicker
+          numberOfMonths={1}
+          selectedDays={[from, { from, to }]}
+          onDayClick={this.handleDayClick}
+          fixedWeeks
+        />
       </div>
     )
   };
