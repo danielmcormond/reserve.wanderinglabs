e444749e4557def31d5a7570b46a9acfaa6eab8c
Tim Watson <tiwatson@gmail.com>
2017-11-22 21:26:16 -0700

    reserve url

--- rails/app/services/reserve_url.rb
+++ rails/app/services/reserve_url.rb
@@ -8,6 +8,25 @@ class ReserveUrl
   end
 
   def url
+    if site.facility.is_a?(Facility::ReserveCalifornia)
+      reserve_california
+    elsif site.facility.is_a?(Facility::RecreationGov)
+      recreation_gov
+    else
+      reserve_america
+    end
+  end
+
+  def reserve_california
+    arvdate = availability_match.avail_date.strftime('%m/%d/%Y')
+    "https://www.reservecalifornia.com/CaliforniaWebHome/Facilities/UnitDetailPopup.aspx?facility_id=#{site.facility.rc_facility_id}&unit_id=#{site.ext_site_id}&arrival_date=#{arvdate}%2012:00:00%20AM&is_available=True"
+  end
+
+  def reserve_america
+
+  end
+
+  def recreation_gov
     arvdate = availability_match.avail_date.strftime('%m/%d/%Y')
     "https://www.recreation.gov/camping/Watchman_Campground/r/campsiteDetails.do?siteId=#{site.ext_site_id}&contractCode=NRSO&parkId=#{site.facility.details['LegacyFacilityID'].to_i}&offset=0&arvdate=#{arvdate}&lengthOfStay=#{availability_request.stay_length}"
   end
