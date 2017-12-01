2c0111832ff6472b30bf3fdf2cb2803e8cf950ed
Tim Watson <tiwatson@gmail.com>
2017-11-30 23:42:47 -0700

    Premium hack..

--- rails/app/serializers/availability_match_serializer.rb
+++ rails/app/serializers/availability_match_serializer.rb
@@ -1,5 +1,6 @@
 class AvailabilityMatchSerializer < ActiveModel::Serializer
-  attributes :id, :avail_date, :length, :available, :unavailable_at, :notified_at, :short, :reserve, :facility, :facility_type
+  attributes :id, :avail_date, :length, :available, :unavailable_at, :notified_at, :short, :reserve, :facility,
+             :facility_type, :premium
   belongs_to :site
 
   def short
@@ -19,5 +20,10 @@ class AvailabilityMatchSerializer < ActiveModel::Serializer
   def facility_type
     return unless @instance_options[:show_reserve].present?
     object.facility.class.to_s
+  end
+
+  def premium
+    return unless @instance_options[:show_reserve].present?
+    object.availability_request.user.premium
   end
 end
--- rails/app/serializers/availability_request_serializer.rb
+++ rails/app/serializers/availability_request_serializer.rb
@@ -1,11 +1,15 @@
 class AvailabilityRequestSerializer < ActiveModel::Serializer
   attributes :uuid, :user_id, :facility_id,
              :date_start, :date_end, :stay_length, :matches_availabile_count,
-             :status, :checked_count, :checked_at
+             :status, :checked_count, :checked_at, :premium
 
   belongs_to :facility
 
   def matches_availabile_count
     object.availability_matches.available.count
+  end
+
+  def premium
+    object.user.premium
   end
 end
