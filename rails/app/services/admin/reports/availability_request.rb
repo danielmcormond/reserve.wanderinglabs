f906334d7d752c841f97b84a98dbf30e3ba7ba4b
Tim Watson <tiwatson@gmail.com>
2017-12-01 17:18:58 -0700

    report update

--- rails/app/services/admin/reports/availability_request.rb
+++ rails/app/services/admin/reports/availability_request.rb
@@ -6,14 +6,17 @@ module Admin::Reports
       items = ::AvailabilityRequest.where('created_at > ?', since).all.map do |a|
         {
           email: a.user.email,
+          premium: a.user.premium,
           facility: a.facility.name,
+          date: "#{a.date_start} - #{a.date_end}",
+          stay_length: a.stay_length,
           checked_count: a.checked_count,
           status: a.status,
           sites: a.site_ids.size,
-          matches: a.availability_matches.count,
+          matches: "#{a.availability_matches.count} / #{a.availability_matches.available.count}",
         }
       end
-      ap items
+      ap(items, indent: 2, index: false, ruby19_syntax: true)
       nil
     end
   end
