c0ffb605856477b5a222fffb5bf430d6a68abe14
Tim Watson <tiwatson@gmail.com>
2017-12-09 00:34:25 -0700

    AR Summary

--- rails/app/serializers/availability_request_serializer.rb
+++ rails/app/serializers/availability_request_serializer.rb
@@ -1,7 +1,7 @@
 class AvailabilityRequestSerializer < ActiveModel::Serializer
   attributes :uuid, :user_id, :facility_id,
              :date_start, :date_end, :stay_length, :matches_availabile_count,
-             :status, :checked_count, :checked_at, :premium
+             :status, :checked_count, :checked_at, :premium, :summary, :site_count
 
   belongs_to :facility
 
@@ -11,5 +11,22 @@ class AvailabilityRequestSerializer < ActiveModel::Serializer
 
   def premium
     object.user.premium
+  end
+
+  def site_count
+    object.site_ids.size
+  end
+
+  def summary
+    params = []
+    params.push(object.site_type.text)
+    params.push('Water') if object.water?
+    params.push('Sewer') if object.sewer?
+    params.push("#{object.min_electric} amps or more") if object.min_electric?
+    params.push("Minumum #{object.min_length}'") if object.min_length?
+    params.push('Pull Thru') if object.pullthru?
+    params.push('Premium sites') if object.site_premium?
+    params.push('Skip ADA') if object.ignore_ada?
+    params.join(', ')
   end
 end
