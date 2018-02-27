cd3272e355887c3424741470c10d962ba52920fd
Tim Watson <tiwatson@gmail.com>
2018-02-26 18:22:59 -0800

    PP

--- rails/config/initializers/paypal.rb
+++ rails/config/initializers/paypal.rb
@@ -1,5 +1,5 @@
 PayPal::SDK.configure(
-  mode: Rails.env.production? ? 'production' : 'sandbox', # TODO - live - (put keys in env)
+  mode: Rails.env.production? ? 'live' : 'sandbox', # TODO - live - (put keys in env)
   client_id: ENV['PAYPAL_ID'],
   client_secret: ENV['PAYPAL_SECRET'],
   ssl_options: {}
