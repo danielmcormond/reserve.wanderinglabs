606c946ef3428533706ad222c20ebfa170599285
Tim Watson <tiwatson@gmail.com>
2018-03-09 16:53:47 -0800

    Site nums

--- rails/app/serializers/availability_import_serializer.rb
+++ rails/app/serializers/availability_import_serializer.rb
@@ -1,5 +1,6 @@
 class AvailabilityImportSerializer < ActiveModel::Serializer
-  attributes :id, :created_at, :date_start, :date_end, :history_open_count, :history_filled_count, :history_open, :history_filled
+  attributes :id, :created_at, :date_start, :date_end, :history_open_count, :history_filled_count,
+             :history_open, :history_filled
 
   belongs_to :facility
 
@@ -11,9 +12,30 @@ class AvailabilityImportSerializer < ActiveModel::Serializer
     end
   end
 
+  def history_open
+    return nil unless object.history_open
+    object.history_open.map do |h|
+      {
+        site_id: Site.find(h['site_id']).site_num,
+        avail_date: h['avail_date'],
+      }
+    end
+  end
+
+  def history_filled
+    return nil unless object.history_filled
+    object.history_filled.map do |h|
+      {
+        site_id: Site.find(h['site_id']).site_num,
+        avail_date: h['avail_date'],
+      }
+    end
+  end
+
   def history_open_count
     object.history_open&.size || 0
   end
+
   def history_filled_count
     object.history_filled&.size || 0
   end
