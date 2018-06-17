0500818c42ed390cc1b40a659f25bca83a2fe1f5
Tim Watson <tiwatson@gmail.com>
2018-06-17 16:53:34 -0700

    misc

--- scrapers/reserve_ca/readme.txt
+++ scrapers/reserve_ca/readme.txt
@@ -7,6 +7,9 @@ docker run -d -p 3003:3003 --env-file ./.env wanderinglabs/scraper:camis.0.4
 curl -d '{"facilityId":"5041", "rcFacilityId":"694", "rcPlaceId":"715"}' -H "Content-Type: application/json" -X POST http://localhost:3003/scrape
 curl -d '{"facilityId":"5041", "rcFacilityId":"694", "rcPlaceId":"715"}' -H "Content-Type: application/json" -X POST http://204.48.25.66:81/scrape
 
+curl -d "{\"facilityId\":4958,\"rcFacilityId\":2016,\"rcPlaceId\":686,\"hash\":null}" -H "Content-Type: application/json" -X POST http://localhost:3003/scrape
+
+
 
 # PRD
 docker pull 302062960021.dkr.ecr.us-east-1.amazonaws.com/wanderinglabs/scraper-reserve_ca:latest
--- scrapers/reserve_ca/src/scraper.js
+++ scrapers/reserve_ca/src/scraper.js
@@ -10,8 +10,6 @@ import { Parse } from './parse';
 import { S3Save } from './s3_save';
 import { NotifyComplete } from './notify_complete';
 
-import Slack from './slack'
-
 class Scraper {
   constructor(params) {
     this.facilityId = params["facilityId"];
@@ -34,19 +32,11 @@ class Scraper {
       let timingsStart = +new Date();
 
       return Promise.mapSeries(timePeriods, (periodStart) => {
-        // console.log(this.logPrefix, 'periodStart', periodStart)
         return this.get_and_parse(periodStart);
-        // return retry(this.get_and_parse, { args: [periodStart], context: this, interval: 250 });
       }).then((results) => {
-        let timingsEnd = +new Date();
-        let duration = timingsEnd - timingsStart;
-        let logging = `\nTot: ${this.timings.length}\nDur: ${duration}\nMin: ${_.min(this.timings)}\nMax: ${_.max(this.timings)}\nMean: ${_.mean(this.timings)}\nSum: ${_.sum(this.timings)}`;
-
         let filtered_results = this.filter_results(results);
         let results_json = this.results_json(filtered_results);
         let md5 = this.results_hash(results_json);
-
-        console.log(this.logPrefix, 'filtered_results', Object.keys(filtered_results).length)
 
         if (this.hash === md5) {
           console.log(this.logPrefix, 'No Differences', md5)
@@ -76,47 +66,35 @@ class Scraper {
   get_and_parse(iter) {
     return this.nextDate(iter).then(() => {
       return this.connection.grid().then((result) => {
-        this.timings.push(result.elapsedTime);
         if (result.body.length === 0) {
           return Promise.reject(e)
         }
         else {
           return new Parse(result.body.d).do().then((matches) => {
-            console.log(this.logPrefix, iter, matches.length)
             return [matches];
           });
         }
       }).catch((e) => {
         console.log(this.logPrefix, iter, 'Failure', e.message);
-
-        // if (e.message === 'Error: ESOCKETTIMEDOUT') {
-        //   console.log(this.logPrefix, iter, 'TIMEOUT');
-        // }
-        // else {
-        //   console.log(e);
-        // }
         return Promise.reject(e)
-
       });
     });
   }
 
   filter_results(results) {
     var filtered = _.flatten(_.flatten(results))
-    filtered = _.uniqWith(filtered, _.isEqual);
 
     var correctHash = {};
     _.forEach(filtered, function(value) {
       correctHash[value[1]] = correctHash[value[1]] || [];
-      correctHash[value[1]].push(value[0])
+      if (correctHash[value[1]].includes(value[0]) !== true) {
+        correctHash[value[1]].push(value[0])
+      }
     });
     return correctHash;
   }
 
   results_json(filtered_results) {
-    // console.log('filtered_results to json', filtered_results.length)
-    let huh = JSON.stringify(filtered_results)
-    // console.log('filtered_results to json', huh)
     return JSON.stringify({ results: filtered_results });
   }
 
