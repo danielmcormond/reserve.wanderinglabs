07b35a49cfd19db655f80eea7a88ed498b26eca1
Tim Watson <tiwatson@gmail.com>
2019-01-10 11:45:52 -0700

    SessionAction tests

--- front-end/src/actions/sessionActions.js
+++ front-end/src/actions/sessionActions.js
@@ -3,75 +3,73 @@ import { push } from "connected-react-router";
 import reserveApi from "../utils/axios";
 import { setFlashMessage } from "../actions/flashActions";
 
-export function sessionNew(email) {
+export const sessionNew = email => {
   return dispatch =>
     reserveApi({
       method: "post",
       url: "/login_tokens",
       data: { email }
     })
-      .then(function(response) {
+      .then(() => {
         dispatch(push("/"));
         dispatch(setFlashMessage("Being emailed", "success"));
       })
-      .catch(function(error) {
+      .catch(() => {
         dispatch(push("/"));
         dispatch(
           setFlashMessage("Email not found. Create a new request.", "error")
         );
       });
-}
+};
 
-export function sessionCreate(token, redirect = null) {
+export const sessionCreate = (token, redirect = null) => {
   return dispatch =>
     reserveApi({
       method: "post",
       url: "/sessions",
       data: { token }
     })
-      .then(function(response) {
+      .then(response => {
         dispatch(sessionSuccess(response.data.auth_token));
         dispatch({ type: "FETCH_USER_FULFILLED", payload: response.data.user });
         dispatch(push(redirect || "/"));
         // dispatch(setFlashMessage("logged in", "success"));
       })
-      .catch(function(error) {
+      .catch(() => {
         dispatch(push("/sign-in"));
         dispatch(setFlashMessage("Bad Token", "error"));
       });
-}
+};
 
-export function sessionCreateNoRedirect(token) {
+export const sessionCreateNoRedirect = token => {
   return dispatch =>
     reserveApi({
       method: "post",
       url: "/sessions",
       data: { token }
     })
-      .then(function(response) {
+      .then(response => {
         dispatch(sessionSuccess(response.data.auth_token));
         dispatch({ type: "FETCH_USER_FULFILLED", payload: response.data.user });
       })
-      .catch(function(error) {
+      .catch(() => {
         dispatch(setFlashMessage("Bad Token", "error"));
       });
-}
+};
 
-export function sessionDestroy(token) {
-  return function(dispatch) {
+export const sessionDestroy = () => {
+  return dispatch => {
     localStorage.removeItem("token");
     dispatch(sessionSuccess(null));
     dispatch(push("/"));
     dispatch(setFlashMessage("logged out", "success"));
   };
-}
+};
 
-export function sessionSuccess(token) {
+export const sessionSuccess = token => {
   localStorage.setItem("token", token);
   return {
     type: "SESSION_SUCCESS",
-    payload: {
-      token: token
-    }
+    payload: { token }
   };
-}
+};
---
+++ front-end/src/actions/sessionActions.test.js
@@ -0,0 +1,158 @@
+import configureMockStore from "redux-mock-store";
+import thunk from "redux-thunk";
+import MockAdapter from "axios-mock-adapter";
+
+import reserveApi from "../utils/axios";
+import {
+  sessionNew,
+  sessionCreate,
+  sessionSuccess,
+  sessionDestroy
+} from "./sessionActions";
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+const mockHttp = new MockAdapter(reserveApi);
+
+describe("sessionSuccess action", () => {
+  const token = "abc123";
+  const store = mockStore({});
+
+  it("stores token in localstorage", () => {
+    store.dispatch(sessionSuccess(token));
+    expect(localStorage.getItem("token")).toEqual(token);
+
+    const expectedPayload = [
+      {
+        type: "SESSION_SUCCESS",
+        payload: { token }
+      }
+    ];
+    expect(store.getActions()).toEqual(expectedPayload);
+  });
+});
+
+describe("sessionDestroy action", () => {
+  const token = null;
+  const store = mockStore({});
+
+  it("removes token localstorage and resets undef", () => {
+    store.dispatch(sessionDestroy());
+    expect(localStorage.getItem("token")).toEqual("null");
+
+    const expectedPayload = [
+      {
+        type: "SESSION_SUCCESS",
+        payload: { token }
+      },
+      {
+        payload: { args: ["/"], method: "push" },
+        type: "@@router/CALL_HISTORY_METHOD"
+      },
+      {
+        payload: { message: "logged out", style: "success" },
+        type: "FLASH_MESSAGE"
+      }
+    ];
+    expect(store.getActions()).toEqual(expectedPayload);
+  });
+});
+
+describe("sessionNew action", () => {
+  const response = {};
+
+  it("redirects to home page and sets flash message upon success", () => {
+    const store = mockStore({});
+    const expectedPayload = [
+      {
+        payload: { args: ["/"], method: "push" },
+        type: "@@router/CALL_HISTORY_METHOD"
+      },
+      {
+        payload: { message: "Being emailed", style: "success" },
+        type: "FLASH_MESSAGE"
+      }
+    ];
+
+    const email = "foo@example.com";
+    mockHttp.onPost("login_tokens", { email }).reply(200, response);
+
+    return store.dispatch(sessionNew(email)).then(() => {
+      expect(store.getActions()).toEqual(expectedPayload);
+    });
+  });
+
+  it("redirects to home page and sets flash message upon not found", () => {
+    const store = mockStore({});
+    const expectedPayload = [
+      {
+        payload: { args: ["/"], method: "push" },
+        type: "@@router/CALL_HISTORY_METHOD"
+      },
+      {
+        payload: {
+          message: "Email not found. Create a new request.",
+          style: "error"
+        },
+        type: "FLASH_MESSAGE"
+      }
+    ];
+
+    const email = "foo@example.com";
+    mockHttp.onPost("login_tokens", { email }).reply(404, response);
+
+    return store.dispatch(sessionNew(email)).then(() => {
+      expect(store.getActions()).toEqual(expectedPayload);
+    });
+  });
+});
+
+describe("sessionCreate action", () => {
+  it("redirects to home page and sets flash message upon success", () => {
+    const store = mockStore({});
+    const expectedPayload = [
+      { payload: { token: "barfoo" }, type: "SESSION_SUCCESS" },
+      { payload: { email: "foo@example.com" }, type: "FETCH_USER_FULFILLED" },
+      {
+        payload: { args: ["/"], method: "push" },
+        type: "@@router/CALL_HISTORY_METHOD"
+      }
+    ];
+
+    const token = "foobar";
+    const response = {
+      auth_token: "barfoo",
+      user: { email: "foo@example.com" }
+    };
+    mockHttp.onPost("sessions", { token }).reply(200, response);
+
+    return store.dispatch(sessionCreate(token)).then(() => {
+      expect(store.getActions()).toEqual(expectedPayload);
+    });
+  });
+
+  it("redirects to sign in page and sets flash message upon bad token", () => {
+    const store = mockStore({});
+    const expectedPayload = [
+      {
+        payload: { args: ["/sign-in"], method: "push" },
+        type: "@@router/CALL_HISTORY_METHOD"
+      },
+      {
+        payload: {
+          message: "Bad Token",
+          style: "error"
+        },
+        type: "FLASH_MESSAGE"
+      }
+    ];
+
+    const token = "foobar";
+    const response = {};
+    mockHttp.onPost("sessions", { token }).reply(401, response);
+
+    return store.dispatch(sessionCreate(token)).then(() => {
+      expect(store.getActions()).toEqual(expectedPayload);
+    });
+  });
+});
