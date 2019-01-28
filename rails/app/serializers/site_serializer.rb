1f48fd8ff28d1a53f045fceb3f855921005a2630
Tim Watson <tiwatson@gmail.com>
2019-01-28 15:45:28 -0700

    Site details can be an array

--- rails/app/serializers/site_serializer.rb
+++ rails/app/serializers/site_serializer.rb
@@ -2,10 +2,14 @@ class SiteSerializer < ActiveModel::Serializer
   attributes :id, :site_num, :site_type, :water, :sewer, :electric, :length, :loop, :site_type2, :ada, :premium
 
   def loop
+    return unless object.details.is_a?(Hash)
+
     object.details.try(:[], 'Loop')
   end
 
   def site_type2
+    return unless object.details.is_a?(Hash)
+
     object.details.try(:[], 'SiteType')
   end
 end
--- rails/spec/serializers/site_serializer_spec.rb
+++ rails/spec/serializers/site_serializer_spec.rb
@@ -1,10 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe SiteSerializer do
-  let(:site) { FactoryGirl.build(:site) }
+  let(:site) { FactoryGirl.build(:site, details: { 'SiteType' => 'foobar' }) }
   let(:serializer) { SiteSerializer.new(site) }
 
   it 'contains site_num' do
     expect(JSON.parse(serializer.to_json)['site_num']).to eq(site.site_num)
+  end
+
+  it 'contains site_type2' do
+    expect(JSON.parse(serializer.to_json)['site_type2']).to eq('foobar')
+  end
+
+  context 'site details is an array' do
+    let(:site) { FactoryGirl.build(:site, details: []) }
+    it 'still contains site_num' do
+      expect(JSON.parse(serializer.to_json)['site_num']).to eq(site.site_num)
+    end
   end
 end
