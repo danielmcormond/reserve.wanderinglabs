2b62743cc27cf201cc382f6fd6364686f2e0c05e
Tim Watson <tiwatson@gmail.com>
2019-01-28 11:37:00 -0700

    Remove unused reserve_link from site model

--- rails/app/models/site.rb
+++ rails/app/models/site.rb
@@ -4,16 +4,16 @@ class Site < ApplicationRecord
   has_many :availability_matches, dependent: :destroy
   belongs_to :facility
 
-  enumerize :site_type, in: %i[group tent_walk_in tent other rv], predicates: { prefix: true }
-  enumerize :site_layout, in: %i[back_in pull_thru other head_in hike_in], predicates: { prefix: true }
+  enumerize :site_type,
+            in: %i[group tent_walk_in tent other rv],
+            predicates: { prefix: true }
+
+  enumerize :site_layout,
+            in: %i[back_in pull_thru other head_in hike_in],
+            predicates: { prefix: true }
 
   scope :lookup, (->(start) { where('site_num ILIKE ?', "%#{start}%").order('site_num ASC').limit(25) })
 
   scope :electric, (->(lngth) { where('electric >= ?', lngth) })
   scope :site_length, (->(lngth) { where('length >= ?', lngth) })
-
-  def reserve_link(date, length)
-    arvdate = date.strftime('%m/%d/%Y')
-    "https://www.recreation.gov/camping/Watchman_Campground/r/campsiteDetails.do?siteId=#{ext_site_id}&contractCode=NRSO&parkId=#{facility.details['LegacyFacilityID'].to_i}&offset=0&arvdate=#{arvdate}&lengthOfStay=#{length}"
-  end
 end
