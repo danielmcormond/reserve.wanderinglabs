a93c48deed9a0fa200d16439f2ca1aa8a6861641
Tim Watson <tiwatson@gmail.com>
2020-03-19 21:06:52 -0700

    Dont notify if avail already recently notified

--- rails/app/models/availability_match.rb
+++ rails/app/models/availability_match.rb
@@ -7,6 +7,23 @@ class AvailabilityMatch < ApplicationRecord
   scope :notifiable, (-> { where(notified_at: nil) })
   scope :notified, (-> { where.not(notified_at: nil) })
 
+  scope :not_recently_notified, (-> {
+    where(
+      <<~SQL
+        EXISTS(
+          SELECT 1 FROM availability_matches AS am2
+          WHERE
+          availability_matches.availability_request_id = am2.availability_request_id AND
+          availability_matches.site_id = am2.site_id AND
+          availability_matches.avail_date = am2.avail_date AND
+          availability_matches.id != am2.id AND
+          am2.notified_at IS NOT NULL AND
+          am2.notified_at < now() - interval '30 minutes'
+        )
+      SQL
+    )
+  })
+
   def facility
     site.facility
   end
--- rails/spec/models/availability_match_spec.rb
+++ rails/spec/models/availability_match_spec.rb
@@ -1,13 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe AvailabilityMatch, type: :model do
-  let(:match) { FactoryGirl.create(:availability_match) }
+  let(:site) { create(:site) }
+  let(:ar) { FactoryGirl.create :availability_request }
+  let!(:match) { FactoryGirl.create(:availability_match, availability_request: ar, site: site, avail_date: Date.current.advance(days: 3), notified_at: 20.minutes.ago) }
 
   describe '#find_by_base62' do
     it 'given base62 returns the instance' do
       base62 = Base62.encode(match.id)
       instance = AvailabilityMatch.find_by_base62(base62)
       expect(instance.id).to eq(match.id)
+    end
+  end
+
+  describe ".not_recently_notified" do
+    let!(:match2) { FactoryGirl.create(:availability_match, availability_request: ar, site: site, avail_date: Date.current.advance(days: 3)) }
+
+    it 'does not return the id if the same avail had previously been notified' do
+      expect(AvailabilityMatch.notifiable.not_recently_notified.count).to eq(0)
+    end
+
+    context 'when the notified_at time of the previous is greater than 30 minutes' do
+      let!(:match) { FactoryGirl.create(:availability_match, availability_request: ar, site: site, avail_date: Date.current.advance(days: 3), notified_at: 45.minutes.ago) }
+
+      it 'does return the id of the new match' do
+        expect(AvailabilityMatch.notifiable.not_recently_notified.count).to eq(1)
+        expect(AvailabilityMatch.notifiable.not_recently_notified.first.id).to eq(match2.id)
+      end
     end
   end
 end
