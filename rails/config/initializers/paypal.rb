3d0200747b394ad92c509b654598e6e19d6dc178
Tim Watson <tiwatson@gmail.com>
2020-08-13 16:50:48 -0600

    PayPal ssl error attempt

--- rails/config/initializers/paypal.rb
+++ rails/config/initializers/paypal.rb
@@ -2,5 +2,5 @@ PayPal::SDK.configure(
   mode: Rails.env.production? ? 'live' : 'sandbox', # TODO - live - (put keys in env)
   client_id: ENV['PAYPAL_ID'],
   client_secret: ENV['PAYPAL_SECRET'],
-  ssl_options: {}
+  ssl_options: { ca_file: nil },
 )
