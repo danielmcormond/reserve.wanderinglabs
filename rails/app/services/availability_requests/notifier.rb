7ea6a6640468c61813b3d470b9a1c97d9a82d334
Tim Watson <tiwatson@gmail.com>
2020-10-30 22:15:05 -0700

    Notification Metrics

--- rails/app/services/availability_requests/notifier.rb
+++ rails/app/services/availability_requests/notifier.rb
@@ -51,6 +51,7 @@ module AvailabilityRequests
         matches_new: availability_request.available_matches.where(notified_at: nil).count,
       )
       availability_request.user.sms_cache if nm.notification_type == :sms
+      ::NewRelic::Agent.increment_metric("Notification/#{nm.allow? ? 'Live' : 'Throttled'}/#{nm.notifcation_type}")
       ln
     end
   end
