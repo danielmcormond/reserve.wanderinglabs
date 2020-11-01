6c0e619a02514b7e7cb0aa8bb49acbe31b1d4db1
Tim Watson <tiwatson@gmail.com>
2020-11-01 10:25:06 -0700

    Remove hardcoding of reserve params

--- rails/app/services/reserve_url.rb
+++ rails/app/services/reserve_url.rb
@@ -5,24 +5,15 @@ class ReserveUrl
     @availability_match = availability_match
   end
 
-  def params
-    if availability_match.site.facility.is_a?(Facility::ReserveCalifornia)
-      Reserve::ReserveCalifornia.new(availability_match).params
-    elsif availability_match.site.facility.is_a?(Facility::RecreationGov)
-      Reserve::RecreationGov.new(availability_match).params
-    elsif availability_match.site.facility.is_a?(Facility::RecreationGovBa)
-      Reserve::RecreationGovBa.new(availability_match).params
-    elsif availability_match.site.facility.is_a?(Facility::Camis)
-      Reserve::Camis.new(availability_match).params
-    elsif availability_match.site.facility.is_a?(Facility::Rec1)
-      Reserve::Rec1.new(availability_match).params
-    else
-      Reserve::ReserveAmerica.new(availability_match).params
-    end
+  def facility
+    @facility ||= availability_match.facility
   end
 
-  def recreation_gov
-    arvdate = availability_match.avail_date.strftime('%m/%d/%Y')
-    "https://www.recreation.gov/camping/Watchman_Campground/r/campsiteDetails.do?siteId=#{site.ext_site_id}&contractCode=NRSO&parkId=#{site.facility.details['LegacyFacilityID'].to_i}&offset=0&arvdate=#{arvdate}&lengthOfStay=#{availability_request.stay_length}"
+  def reserve_class
+    "Reserve::#{facility.class.name.split("::").last}".constantize
+  end
+
+  def params
+    reserve_class.new(availability_match).params
   end
 end
