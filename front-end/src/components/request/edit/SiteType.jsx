27d6ca1f690b9d8b92184b46543652fa18942977
Tim Watson <tiwatson@gmail.com>
2021-01-19 20:42:21 -0700

    SiteType tweaks

--- front-end/src/components/request/edit/SiteType.jsx
+++ front-end/src/components/request/edit/SiteType.jsx
@@ -12,6 +12,7 @@ dayjs.extend(localeData)
 const siteTypeOptions = [
   {
     text: 'RV Sites',
+    short: 'RV Sites',
     description: "Only sites suitable for RV's",
     value: 'rv',
     values: ['rv'],
@@ -19,6 +20,7 @@ const siteTypeOptions = [
   },
   {
     text: 'Tent or RV',
+    short: 'Tent/RV',
     description: 'You can tent in an RV site',
     value: 'rv_tent',
     values: ['rv', 'tent'],
@@ -26,6 +28,7 @@ const siteTypeOptions = [
   },
   {
     text: 'Tents Only',
+    short: 'Tents',
     description: "No RV's",
     value: 'tent',
     values: ['tent', 'tent_walk_in'],
@@ -33,6 +36,7 @@ const siteTypeOptions = [
   },
   {
     text: 'Group Sites',
+    short: 'Groups',
     description: 'You and your friends',
     value: 'group',
     values: ['group'],
@@ -40,6 +44,7 @@ const siteTypeOptions = [
   },
   {
     text: 'Other',
+    short: 'Other',
     description: 'Cabins, Day Use, etc',
     value: 'other',
     values: ['other'],
@@ -83,8 +88,11 @@ const SiteType = ({ dispatch, facility, type }) => {
             `}
             onClick={() => handleTypeClick(option.value)}
           >
-            <FontAwesomeIcon icon={option.icon} className="text-xl sm:text-3xl md:text-6xl" />
-            <h3 className="mt-1 md:mt-2 font-semibold tracking-tighter md:tracking-tight text-sm">{option.text}</h3>
+            <FontAwesomeIcon icon={option.icon} className="text-xl sm:text-3xl md:text-6xl mt-1" />
+            <h3 className="mt-1 md:mt-2 font-semibold tracking-tighter md:tracking-tight text-sm">
+              <span className="hidden sm:inline">{option.text}</span>
+              <span className="inline sm:hidden">{option.short}</span>
+            </h3>
           </div>
         </div>
       ))}
