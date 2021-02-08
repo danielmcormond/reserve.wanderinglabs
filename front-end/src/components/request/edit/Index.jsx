45725bc85eccffb17dd0a42debfc7d46d2fa6d4c
Tim Watson <tiwatson@gmail.com>
2021-02-07 17:15:02 -0700

    Move to reducer

--- front-end/src/components/request/edit/Index.jsx
+++ front-end/src/components/request/edit/Index.jsx
@@ -1,8 +1,5 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { actions } from 'react-redux-form'
-import dayjs from 'dayjs'
-import localeData from 'dayjs/plugin/localeData'
 
 import { fetchAvailabilityRequest } from '../../../actions/availabilityRequestsActions'
 import { formStepGo } from '../../../actions/requestFormActions'
@@ -25,29 +22,9 @@ const RequestEdit = ({ match }) => {
 
   useEffect(() => {
     if (ar && ar.facilityId) {
-      dispatch(actions.change('availabilityRequestForm.facilityId', ar.facilityId))
-
       if (currentStep === 1) {
         dispatch(formStepGo(2))
       }
-
-      dispatch(actions.change('availabilityRequestForm.uuid', ar.uuid))
-      dispatch(actions.change('availabilityRequestForm.siteIds', ar.siteIds))
-      dispatch(actions.change('availabilityRequestForm.dateEnd', dayjs(ar.dateEnd).toDate()))
-      dispatch(actions.change('availabilityRequestForm.dateStart', dayjs(ar.dateStart).toDate()))
-      dispatch(actions.change('availabilityRequestForm.facility', ar.facility))
-      dispatch(actions.change('availabilityRequestForm.notifySms', ar.notifySms))
-      dispatch(actions.change('availabilityRequestForm.premium', ar.premium))
-      dispatch(actions.change('availabilityRequestForm.stayLength', ar.stayLength))
-      dispatch(actions.change('availabilityRequestForm.specificSiteIds', ar.specificSiteIds))
-      dispatch(actions.change('availabilityRequestForm.length', ar.minLength))
-      dispatch(actions.change('availabilityRequestForm.water', ar.water))
-      dispatch(actions.change('availabilityRequestForm.sewer', ar.sewer))
-      dispatch(actions.change('availabilityRequestForm.pullthru', ar.pullthru))
-      dispatch(actions.change('availabilityRequestForm.electric', ar.minElectric && ar.minElectric.toString()))
-      dispatch(actions.change('availabilityRequestForm.sitePremium', ar.sitePremium))
-      dispatch(actions.change('availabilityRequestForm.ignoreAda', ar.ignoreAda))
-      dispatch(actions.change('availabilityRequestForm.arrivalDays', ar.arrivalDays))
     }
   }, [ar])
 
--- front-end/src/reducers/availabilityRequestFormReducer.js
+++ front-end/src/reducers/availabilityRequestFormReducer.js
@@ -1,3 +1,6 @@
+import camelcaseKeys from 'camelcase-keys'
+import dayjs from 'dayjs'
+
 const initialState = {
   facility: {},
   facilityId: '',
@@ -22,6 +25,30 @@ export default (state = initialState, { type, payload }) => {
   switch (type) {
     case 'SET_PREMIUM': {
       return { ...state, premium: true }
+    }
+    case "FETCH_AR_FULFILLED": {
+      const ar = camelcaseKeys(payload)
+      return {
+        ...state,
+        uuid: ar.uuid,
+        facility: ar.facility,
+        facilityId: ar.facilityId,
+        type: ar.type,
+        dateEnd: dayjs(ar.dateEnd).toDate(),
+        dateStart: dayjs(ar.dateStart).toDate(),
+        notifySms: ar.notifySms,
+        premium: ar.premium,
+        stayLength: ar.stayLength,
+        specificSiteIds: ar.specificSiteIds,
+        length: ar.minLength,
+        water: ar.water,
+        sewer: ar.sewer,
+        pullthru: ar.pullthru,
+        electric: ar.minElectric && ar.minElectric.toString(),
+        sitePremium: ar.sitePremium,
+        ignoreAda: ar.ignoreAda,
+        arrivalDays: ar.arrivalDays
+      };
     }
     default: {
       return { ...state }
